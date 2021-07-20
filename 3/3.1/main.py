368c3f167a4a3ac294716f6e6066e1394478fcfe
ysl2 <songli.yu@outlook.com>
2021-07-20 15:06:51 +0800

    Some change

--- 3/3.1/main.py
+++ 3/3.1/main.py
@@ -5,6 +5,7 @@ import numpy as np
 import torch
 from d2l import torch as d2l
 import matplotlib.pyplot as plt
+import os
 
 # 测试导入上级目录中的包
 # import sys
@@ -42,6 +43,7 @@ class Timer:
         """
         return np.array(self.times).cumsum().tolist()
 
+
 c = torch.zeros(n)
 timer = Timer()
 for i in range(n):
@@ -52,14 +54,22 @@ timer.start()
 d = a + b
 print(f'{timer.stop():.5f} sec')
 
+
 def normal(x, mu, sigma):
     p = 1 / math.sqrt(2 * math.pi * sigma ** 2)
     return p * np.exp(-0.5 / sigma ** 2 * (x - mu) ** 2)
 
+
 x = np.arange(-7, 7, 0.01)
 params = [(0, 1), (0, 2), (3, 1)]
-d2l.plot(x, [normal(x, mu, sigma) for mu, sigma in params], xlabel='x', ylabel='p(x)', figsize=(4.5, 2.5), legend=[f'mean {mu}, std {sigma}' for mu, sigma in params])
+d2l.plot(x, [normal(x, mu, sigma) for mu, sigma in params], xlabel='x', ylabel='p(x)',
+         figsize=(4.5, 2.5), legend=[f'mean {mu}, std {sigma}' for mu, sigma in params])
 
-# plt.savefig("image/graph.png")
-plt.show()
+pic_dir = './image'
+pic_name = 'graph.png'
+pic_path = os.path.join(pic_dir, pic_name)
+os.makedirs(pic_dir, exist_ok=True)
+plt.savefig(pic_path)
+
+# plt.show()
 
