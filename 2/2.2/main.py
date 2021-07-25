6f0152d2f0626d0c69de6ab75f3c86409c699f4e
ysl2 <songli.yu@outlook.com>
2021-07-25 22:03:42 +0800

    Update

---
+++ 2/2.2/main.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+import os
+import pandas as pd
+
+# ---
+data_dir = os.path.join('.', 'data')
+data_name = 'house_tiny.csv'
+data_file = os.path.join(data_dir, data_name)
+# os.makedirs(data_dir, exist_ok=True)
+# with open(data_file, 'w') as f:
+#     f.write('NumRooms,Alley,Price\n')
+#     f.write('NA,Pave,127500\n')
+#     f.write('2,NA,106000\n')
+#     f.write('4,NA,178100\n')
+#     f.write('NA,NA,1410000\n')
+# ---
+
+# ---
+# data = pd.read_csv(data_file)
+# print(data)
+# ---
+
+# ---
+# TODO 2.2.2
+# ---
