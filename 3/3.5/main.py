fda73f9af6cc930aa2d65d224576ef4889c0bd2b
ysl2 <songli.yu@outlook.com>
2021-07-29 17:43:38 +0800

    Restruct folders

---
+++ 3/3.5/main.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.11.4
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# +
+%matplotlib inline
+import torch
+import torchvision
+from torch.utils import data
+from torchvision import transforms
+from d2l import torch as d2l
+
+d2l.use_svg_display()
+# -
+
+
+# +
+trans = transforms.ToTensor()
+mnist_train = torchvision.datasets.FashionMNIST(root='../../data', train=True, download=True)
+mnist_test = 
+# -
