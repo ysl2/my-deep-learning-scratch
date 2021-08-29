d09c363377dca4edf34d2f687b3d0715fa2cfa62
ysl2 <songli.yu@outlook.com>
2021-08-29 14:00:27 +0800

    Add ch.06 lenet

--- 6/lenet/body.py
+++ 6/lenet/body.py
@@ -43,9 +43,9 @@ def load_data_fashion_mnist(batch_size, resize=None):
         trans.insert(0, transforms.Resize(resize))
     trans = transforms.Compose(trans)
     mnist_train = torchvision.datasets.FashionMNIST(
-        root='../data', train=True, transform=trans, download=True)
+        root='~/Documents/_Github/my-deep-learning-scratch/data', train=True, transform=trans, download=True)
     mnist_test = torchvision.datasets.FashionMNIST(
-        root='../data', train=False, transform=trans, download=True)
+        root='~/Documents/_Github/my-deep-learning-scratch/data', train=False, transform=trans, download=True)
     return (data.DataLoader(mnist_train, batch_size, shuffle=True, num_workers=get_dataloader_workers()),
             data.DataLoader(mnist_test, batch_size, shuffle=False, num_workers=get_dataloader_workers()))
 
@@ -205,7 +205,7 @@ def train_ch6(net, train_iter, test_iter, num_epochs, lr, device):
             if (i + 1) % (num_batches // 5) == 0 or i == num_batches - 1:
                 animator.add(epoch + (i + 1) / num_batches,
                             (train_l, train_acc, None))
-                plt.savefig('./fig.png')
+                # plt.savefig('./fig.png')
 
         test_acc = evaluate_accuracy_gpu(net, test_iter)
         animator.add(epoch + 1, (None, None, test_acc))
@@ -213,7 +213,7 @@ def train_ch6(net, train_iter, test_iter, num_epochs, lr, device):
           f'test acc {test_acc:.3f}')
     print(f'{metric[2] * num_epochs / timer.sum():.1f} examples/sec '
           f'on {str(device)}')
-    plt.savefig('./fig.png')
+    # plt.savefig('./fig.png')
 
 
 def try_gpu(i=0):
