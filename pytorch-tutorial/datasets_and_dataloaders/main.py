a4d6b986ebbbdad67ec590062c9186b67d3ced58
ysl2 <songli.yu@outlook.com>
2021-09-26 21:14:10 +0800

    Update pytorch tutorial

--- pytorch-tutorial/datasets_and_dataloaders/main.py
+++ pytorch-tutorial/datasets_and_dataloaders/main.py
@@ -55,3 +55,60 @@ for i in range(1, cols * rows + 1):
     plt.axis('off')
     plt.imshow(img.squeeze(), cmap='gray')
 plt.show()
+
+
+# ===
+# === Creating a custom Dataset for your files
+# ===
+
+import os
+import pandas as pd
+from torchvision.io import read_image
+
+class CustomImageDataset(Dataset):
+    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+        self.img_labels = pd.read_csv(annotations_file)
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        '''
+        returns the number of samples in out dataset
+        :return:
+        '''
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        image = read_image(img_path)
+        label = self.img_labels.iloc[idx, 1]
+        if self.transform:
+            iamge = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return image, label
+
+
+# ===
+# === Preparing your data for training with DataLoaders
+# ===
+
+from torch.utils.data import DataLoader
+train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
+test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
+
+
+# ===
+# === Iterate through the DataLoader
+# ===
+
+# Display images and label
+train_features, train_labels = next(iter(train_dataloader))
+print(f'Feature batch shape: {train_features.size()}')
+print(f'Labels batch size: {train_labels.size()}')
+img = train_features[0].squeeze()
+label = train_labels[0]
+plt.imshow(img, cmap='gray')
+plt.show()
+print(f'label: {label}')
